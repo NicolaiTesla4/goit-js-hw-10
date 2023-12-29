db8fc659f31f58e8580ff8690c8ab917744ad99a
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-12-28 20:18:32 -0500

    Dcuatro

---
+++ src/api.js
@@ -0,0 +1,24 @@
+const url = 'https://api.thecatapi.com/v1';
+const api_key =
+    `live_Bk4fyL6EwSEs5MEqudELYENPKNvaKLnF0ufMSLgEo3j0cTbEjiPJLgmBojKPRRA2`;
+ /*  'live_9I8Ht6x4adAvhN6KfkVrihkR6u4EA11SPuAsmoucpPDG6L1fSA4XKyCxmOGjNLkh'; */
+
+export function fetchBreeds() {
+  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
